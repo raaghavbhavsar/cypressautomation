05d5dd675ad8974d4783ce838e5aa18434f00cf8
Raghav Bhavsar <raaghavbhavsar@gmail.com>
2024-01-05 01:50:23 +0530

    ProwessAdvisors About Us
    infobox content verification

---
+++ cypress/e2e/TestSpec8.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe('My Second Test Suite', function() 
+{
+ 
+it('My FirstTest case',function() {
+ 
+//About us infobox content verification
+cy.viewport(1280,800)
+cy.visit('https://prowessadvisors.com/about-us/')
+// cy.get('#menu-item-4518 > a > :nth-child(1) > .item_text').click()
+cy.get('.page-header_title').should('be.visible').and('have.text','About Us')
+cy.get('.wgl-infobox').click({multiple: true})
+cy.get('.wgl-infobox_content').invoke('text').then((text)=>{
+    const divTxt = text
+    expect(divTxt).to.contain('two decades')
+})
+
+})
+})
